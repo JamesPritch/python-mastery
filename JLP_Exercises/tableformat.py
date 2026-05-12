0cb07685eb8b113485d05f3b2bca20b5afef5447
James Pritchard <James.Pritchard@domobroadcast.com>
2026-05-12 09:27:06 +0000

    End of 3.8, mixin classes and cooperatice inheritance

--- JLP_Exercises/tableformat.py
+++ JLP_Exercises/tableformat.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+# Classes
 class TableFormatter(ABC):
     @abstractmethod
     def headings(self, headers):
@@ -28,23 +29,70 @@ class HTMLTableFormatter(TableFormatter):
     def row(self, rowdata):
         print('<tr> <td>' + '</td> <td>'.join(str(d) for d in rowdata) + '</td> </tr>')
 
+class ColumnFormatMixin:
+    formats = []
+    def row(self, rowdata):
+        rowdata = [(fmt % d) for fmt, d in zip(self.formats, rowdata)]
+        super().row(rowdata)
 
+class UpperHeadersMixin:
+    def headings(self, headers):
+        super().headings([h.upper() for h in headers])
+
+
+# Functions
 def print_table(data, fields, formatter):
-    if not isinstance(data, TableFormatter):
-        raise TypeError('Expected a TableFormatter')
+    # if not isinstance(data, TableFormatter):
+    #     raise TypeError('Expected a TableFormatter')
     formatter.headings(fields)
     for s in data:
         rowdata = [getattr(s, fieldname) for fieldname in fields]
         formatter.row(rowdata)
 
-def create_formatter(type):
+def create_formatter(type, column_formats = False, upper_headers = False):
+    # Input type verification
     if not isinstance(type, str):
-        raise TypeError('Expected string.')
+        raise TypeError('Expected \'type\' of type string.')
+    elif column_formats and (not isinstance(column_formats, list) or 
+                           len(column_formats) != 3):
+        raise TypeError('Expected \'column_formats\' of type list of length 3.')
+    elif not isinstance(upper_headers, bool):
+        raise TypeError('Expected \'upper_headers\' of type bool.')
+    elif column_formats and upper_headers:
+        raise ValueError('Expect one kwarg, got two.')
+    # Processing
     elif type == 'html':
-        return HTMLTableFormatter()
+        if column_formats:
+            class PortfolioFormatter(ColumnFormatMixin, HTMLTableFormatter):
+                formats = column_formats
+            return PortfolioFormatter()
+        elif upper_headers:
+            class PortfolioFormatter(UpperHeadersMixin, HTMLTableFormatter):
+                pass
+            return PortfolioFormatter()
+        else:
+            return HTMLTableFormatter()
     elif type == 'csv':
-        return CSVTableFormatter()
+        if column_formats:
+            class PortfolioFormatter(ColumnFormatMixin, CSVTableFormatter):
+                formats = column_formats
+            return PortfolioFormatter()
+        elif upper_headers:
+            class PortfolioFormatter(UpperHeadersMixin, CSVTableFormatter):
+                pass
+            return PortfolioFormatter()
+        else:
+            return CSVTableFormatter()
     elif type == 'text':
-        return TextTableFormatter()
+        if column_formats:
+            class PortfolioFormatter(ColumnFormatMixin, TextTableFormatter):
+                formats = column_formats
+            return PortfolioFormatter()
+        elif upper_headers:
+            class PortfolioFormatter(UpperHeadersMixin, TextTableFormatter):
+                pass
+            return PortfolioFormatter()
+        else:
+            return TextTableFormatter()
     else:
         raise ValueError('Expected \'csv\', \'html\', or \'text\'.')
