e17dac81766a5576c2d1b4e71ae3daa9d302bb37
James Pritchard <James.Pritchard@domobroadcast.com>
2026-05-20 09:16:28 +0000

    Exercise 5.1: functions and type hints; tidying pycache.

--- JLP_Exercises/reader.py
+++ JLP_Exercises/reader.py
@@ -1,82 +1,43 @@
 import csv
-import collections
-from sys import intern
-from abc import ABC, abstractmethod
+from typing import List, TextIO
 
-
-# Classes
-class DataCollection(collections.abc.Sequence):
-    def __init__(self):
-        kwargs = collections.defaultdict()
-        for cols in headers:
-            kwargs[cols] = []
-        self.__dict__.update(kwargs)
-    def __len__(self):
-        return len(self.__dict__[headers[0]]) # Assume all lists have equal lengths
-    def __getitem__(self, index):
-        if isinstance(index, int):
-            return {str(col):self.__dict__[cols][index] for col, cols in zip(header, headers)}
-        listofsets = DataCollection()
-        for i in range(index.start, index.stop):
-            listofsets.append({cols: self.__dict__[cols][i] for cols in headers})
-        return listofsets
-    def append(self, d): 
-        for cols in headers:
-            self.__dict__[cols].append(d[cols])
-
-class CSVParser(ABC):
-    def parse(self, filename):
-        records = []
-        with open(filename) as f:
-            rows = csv.reader(f)
-            headers = next(rows)
-            for row in rows:
-                record = self.make_record(headers, row)
-                records.append(record)
-        return records
-    
-    @abstractmethod
-    def make_record(self, headers, row):
-        pass
-
-class DictCSVParser(CSVParser):
-    def __init__(self, types):
-        self.types = types
-    def make_record(self, headers, row):
-        return { name: func(val) for name, func, val in zip(headers, self.types, row) }
-    
-class InstanceCSVParser(CSVParser):
-    def __init__(self, cls):
-        self.cls = cls
-    def make_record(self, headers, row):
-        return self.cls.from_row(row)
-
-
-# Functions
-# Reads a csv file and converts the types of its columns 
-def read_csv_as_dicts(filename, coltypes):
-    parser = DictCSVParser(coltypes)
-    return parser.parse(filename)
-
-
-def read_csv_as_columns(filename, coltypes=[str, str, str, int]):
+def read_csv_as_dicts(filename:str, types:List[type]) -> List[dict]:
     '''
-    Read any csv file with headers in the first row and save them as lists
+    Read CSV data into a list of dictionaries with optional type conversion
     '''
-    with open(filename) as f:
-        global header
-        global headers
-        rows = csv.reader(f)
-        # Assuming headers from the first row
-        header = next(rows)
-        # Turn headers plural, i.e. route -> routes, rides -> numrides
-        headers = ['num' + col if col[-1] == 's' else col + 's' for col in header]
-        records = DataCollection()
-        for row in rows:
-            record = {cols:func(val) for cols, func, val in zip(headers, coltypes, row)}
-            records.append(record)
+    with open(filename) as file:
+        return csv_as_dicts(file, types)
+
+def read_csv_as_instances(filename:str, cls:type) -> List:
+    '''
+    Read CSV data into a list of instances
+    '''
+    with open(filename) as file:
+        return csv_as_instances(file, cls)
+
+def csv_as_dicts(file:TextIO, types:List[type], headers:str = None) -> List[dict]:
+    '''
+    Read CSV data into a list of dictionaries with optional type conversion
+    '''
+    records = []
+    rows = csv.reader(file)
+    if not headers:
+        headers = next(rows)
+    for row in rows:
+        record = { name: func(val) 
+                    for name, func, val in zip(headers, types, row) }
+        records.append(record)
     return records
 
-def read_csv_as_instances(filename, cls):
-    parser = InstanceCSVParser(cls)
-    return parser.parse(filename)
+def csv_as_instances(file:TextIO, cls:type, headers:str = None) -> List:
+    '''
+    Read CSV data into a list of instances
+    '''
+    records = []
+    rows = csv.reader(file)
+    if not headers:
+        headers = next(rows)
+    for row in rows:
+        record = cls.from_row(row)
+        records.append(record)
+    return records
